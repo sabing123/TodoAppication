00d3df39902f726f2acc7156e0d45d6233aea7a5
Sabin <sabingautam05@gmail.com>
2021-02-27 21:11:48 +0545

    aaa

---
+++ app/src/main/java/com/example/todoappication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.todoappication;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.todoappication.fragment.NewFragment;
+import com.example.todoappication.fragment.favouriteFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new NewFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                fragment = new NewFragment();
+                break;
+            case R.id.action_fav:
+                fragment = new favouriteFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
